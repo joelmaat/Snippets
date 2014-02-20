cee664f3317e3239bc25444069f38a680f09473c
Joel Johnson <joelmaat@gmail.com>
2014-02-20 10:14:08 -0500

    Added doc comments

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -2,29 +2,35 @@ import random
 
 
 def are_cells_poorly_formed(cells):
+    "Returns True if cell isn't an int value between 0 and 9 (inclusive)."
     return any(type(cell) is not int or cell < 0 or cell > 9 for cell in cells)
 
 
 def is_row_poorly_formed(row):
+    "Returns True if row isn't a list of 9 element."
     return type(row) is not list or len(row) != 9
 
 
 def is_value_repeated(row):
+    "Returns true if there are duplicate entries in row.."
     row = [cell for cell in row if cell != 0]
     return len(set(row)) != len(row)
 
 
 def get_square(grid, r_index, c_index):
+    "Returns square cell at r_index, c_index belongs to."
     return (grid[r_index][c_index:c_index + 3]
             + grid[r_index + 1][c_index:c_index + 3] 
             + grid[r_index + 2][c_index:c_index + 3])
 
 
 def get_s_index(r_index, c_index):
+    "Returns index of square containing cell at r_index, c_index."
     return (r_index / 3 * 3) + (c_index / 3)
 
 
 def check_sudoku(grid):
+    "Returns None if ill-formed, False if duplicates present, or True otherwise."
     if is_row_poorly_formed(grid) or any(is_row_poorly_formed(row) or are_cells_poorly_formed(row)
                                          for row in grid):
         return None
@@ -35,6 +41,7 @@ def check_sudoku(grid):
 
 
 def get_index_with_least_options(zeros):
+    "Returns index of element with the least number of valid options."
     z_index = best = None
     for index in xrange(len(zeros)):
         num_options = len(zeros[index][0])
@@ -47,6 +54,7 @@ def get_index_with_least_options(zeros):
 
 
 def remove_option(option, zeros, remove_option_from):
+    "Removes given option from each specified index in zeros if present."
     option_set = set([option])
     z_copy = zeros[:]
     for index in remove_option_from:
@@ -56,6 +64,7 @@ def remove_option(option, zeros, remove_option_from):
 
 
 def replace_zeros(grid, zeros):
+    "Replaces blank cells with valid values, or returns False if no solution found."
     if not zeros:
         return grid
     z_index = get_index_with_least_options(zeros)
@@ -74,12 +83,14 @@ def replace_zeros(grid, zeros):
 
 
 def create_metadata(rows, columns, squares, r_index, c_index):
+    "Returns metadata for cell at specified r_index, c_index."
     s_index = get_s_index(r_index, c_index)
     options = rows[r_index] & columns[c_index] & squares[s_index]
     return (options, r_index, c_index, s_index)
 
 
 def find_blank_cells(grid):
+    "Returns indices of all empty cells and a set of valid values for each."
     universe = set(xrange(1, 10))
     rows = [universe - set(row) for row in grid]
     columns = [universe - set(row) for row in zip(*grid)]
@@ -95,12 +106,12 @@ def find_blank_cells(grid):
 
 
 def solve_sudoku(grid):
-    "Grid should be a list of 9 lists, each with 9 ints (range: 0 - 9). 0 for blank cells."
+    "Returns solved grid (expects 9x9 list with 0 for blank cells), or False if no solution found."
     return check_sudoku(grid) and replace_zeros([row[:] for row in grid], find_blank_cells(grid))
 
 
 def generate_puzzle():
-    "About 5-6% will have no solution."
+    "Returns a randomly generated puzzle (about 5-6% will have no solution)."
     dimension = 9
     max_num_cells_to_populate = 17
     puzzle = [[0] * dimension for _ in xrange(dimension)]
