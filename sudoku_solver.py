f3513fcec13262e67cad7b880288144eadb66f83
Joel Johnson <joelmaat@gmail.com>
2014-02-20 09:22:35 -0500

    Changed naming scheme from camelcase to underscore

--- sudoku_solver.py
+++ sudoku_solver.py
@@ -1,113 +1,124 @@
 import random
 
 
-def areCellsPoorlyFormed(cells):
+def are_cells_poorly_formed(cells):
     return any(type(cell) is not int or cell < 0 or cell > 9 for cell in cells)
 
 
-def isRowPoorlyFormed(row):
+def is_row_poorly_formed(row):
     return type(row) is not list or len(row) != 9
 
 
-def isValueRepeated(row):
+def is_value_repeated(row):
     row = [cell for cell in row if cell != 0]
     return len(set(row)) != len(row)
 
 
+def get_square(grid, r_index, c_index):
+    return (grid[r_index][c_index:c_index + 3]
+                + grid[r_index + 1][c_index:c_index + 3]
+                + grid[r_index + 2][c_index:c_index + 3])
+
+
+def get_s_index(r_index, c_index):
+    return (r_index / 3 * 3) + (c_index / 3)
+
+
 def check_sudoku(grid):
-    if isRowPoorlyFormed(grid) or any(isRowPoorlyFormed(row) or areCellsPoorlyFormed(row)
-                                      for row in grid):
+    if is_row_poorly_formed(grid) or any(is_row_poorly_formed(row) or are_cells_poorly_formed(row)
+                                         for row in grid):
         return None
-    return (not any(isValueRepeated(row) for row in grid + zip(*grid)) and
-            not any(isValueRepeated(grid[r][c:c + 3] + grid[r + 1][c:c + 3] + grid[r + 2][c:c + 3])
+    return (not any(is_value_repeated(row) for row in grid + zip(*grid)) and
+            not any(is_value_repeated(get_square(grid, r, c))
                     for r in [0, 3, 6]
                     for c in [0, 3, 6]))
 
 
-def getIndexWithLeastOptions(zeros):
-    zIndex = best = None
+def get_index_with_least_options(zeros):
+    z_index = best = None
     for index in xrange(len(zeros)):
-        numOptions = len(zeros[index][0])
-        if best is None or numOptions < best:
-            if not numOptions:
+        num_options = len(zeros[index][0])
+        if best is None or num_options < best:
+            if not num_options:
                 return False
-            best = numOptions
-            zIndex = index
-    return zIndex
+            best = num_options
+            z_index = index
+    return z_index
 
 
-def removeOption(option, zeros, removeOptionFrom):
-    optionSet = set([option])
-    zCopy = zeros[:]
-    for index in removeOptionFrom:
-        if option in zCopy[index][0]:
-            zCopy[index] = (zCopy[index][0].copy() - optionSet,) + zCopy[index][1:]
-    return zCopy
+def remove_option(option, zeros, remove_option_from):
+    option_set = set([option])
+    z_copy = zeros[:]
+    for index in remove_option_from:
+        if option in z_copy[index][0]:
+            z_copy[index] = (z_copy[index][0].copy() - option_set,) + z_copy[index][1:]
+    return z_copy
 
 
-def replaceZeros(grid, zeros):
+def replace_zeros(grid, zeros):
     if not zeros:
         return grid
-    zIndex = getIndexWithLeastOptions(zeros)
-    if zIndex is False:
+    z_index = get_index_with_least_options(zeros)
+    if z_index is False:
         return False
-    options, rIndex, cIndex, sIndex = zeros.pop(zIndex)
-    removeOptionFrom = [index
-                        for index, (_, r, c, s) in enumerate(zeros)
-                        if r == rIndex or c == cIndex or s == sIndex]
+    options, r_index, c_index, s_index = zeros.pop(z_index)
+    remove_option_from = [index
+                          for index, (_, r, c, s) in enumerate(zeros)
+                          if r == r_index or c == c_index or s == s_index]
     for option in options:
-        grid[rIndex][cIndex] = option
-        solution = replaceZeros(grid, removeOption(option, zeros, removeOptionFrom))
+        grid[r_index][c_index] = option
+        solution = replace_zeros(grid, remove_option(option, zeros, remove_option_from))
         if solution:
             return solution
     return False
 
 
-def createMetadata(rows, columns, squares, rIndex, cIndex):
-    sIndex = (rIndex / 3 * 3) + (cIndex / 3)
-    options = rows[rIndex] & columns[cIndex] & squares[sIndex]
-    return (options, rIndex, cIndex, sIndex)
+def create_metadata(rows, columns, squares, r_index, c_index):
+    s_index = get_s_index(r_index, c_index)
+    options = rows[r_index] & columns[c_index] & squares[s_index]
+    return (options, r_index, c_index, s_index)
 
 
-def findBlankCells(grid):
+def find_blank_cells(grid):
     universe = set(xrange(1, 10))
     rows = [universe - set(row) for row in grid]
     columns = [universe - set(row) for row in zip(*grid)]
-    squares = [universe - set(grid[r][c:c + 3] + grid[r + 1][c:c + 3] + grid[r + 2][c:c + 3])
+    squares = [universe - set(get_square(grid, r, c))
                for r in [0, 3, 6]
                for c in [0, 3, 6]]
     indices = range(9)
     random.shuffle(indices)
-    return [createMetadata(rows, columns, squares, rIndex, cIndex)
-            for rIndex in indices
-            for cIndex in indices
-            if grid[rIndex][cIndex] == 0]
+    return [create_metadata(rows, columns, squares, r_index, c_index)
+            for r_index in indices
+            for c_index in indices
+            if grid[r_index][c_index] == 0]
 
 
 def solve_sudoku(grid):
     "Grid should be a list of 9 lists, each with 9 ints (range: 0 - 9). 0 for blank cells."
-    return check_sudoku(grid) and replaceZeros([row[:] for row in grid], findBlankCells(grid))
+    return check_sudoku(grid) and replace_zeros([row[:] for row in grid], find_blank_cells(grid))
 
 
-def generatePuzzle():
+def generate_puzzle():
     "About 5-6% will have no solution."
     dimension = 9
-    maxNumCellsToPopulate = 17
+    max_num_cells_to_populate = 17
     puzzle = [[0] * dimension for _ in xrange(dimension)]
     available = range(dimension * dimension)
-    rows, columns, squares = [[set(xrange(1, dimension + 1)) for _ in xrange(dimension)] for _ in xrange(3)]
-    for _ in xrange(maxNumCellsToPopulate):
+    rows, columns, squares = [[set(xrange(1, dimension + 1)) for _ in xrange(dimension)]
+                               for _ in xrange(3)]
+    for _ in xrange(max_num_cells_to_populate):
         index = random.randrange(len(available))
         available.pop(index)
-        rIndex = index / dimension
-        cIndex = index % dimension
-        sIndex = (rIndex / 3 * 3) + (cIndex / 3)
-        options = rows[rIndex] & columns[cIndex] & squares[sIndex]
+        r_index = index / dimension
+        c_index = index % dimension
+        s_index = get_s_index(r_index, c_index)
+        options = rows[r_index] & columns[c_index] & squares[s_index]
         if len(options) < 1:
             break
         value = random.choice(list(options))
-        puzzle[rIndex][cIndex] = value
-        any(cache.discard(value) for cache in [rows[rIndex], columns[cIndex], squares[sIndex]])
+        puzzle[r_index][c_index] = value
+        any(cache.discard(value) for cache in [rows[r_index], columns[c_index], squares[s_index]])
     return puzzle
 
-# for _ in xrange(100): print solve_sudoku(generatePuzzle())
+# print solve_sudoku(generate_puzzle())
