538bf0e887e7658addb20f3ba27689160e350197
Joel Johnson <joelmaat@gmail.com>
2014-02-22 21:24:02 -0500

    Shorten line length

--- permutations_knuth.py
+++ permutations_knuth.py
@@ -8,15 +8,25 @@ def permutations(string):
     string = ''.join(sorted(string))
     length = len(string)
     while True:
-        yield string        
-        left = next((index - 1 for index in xrange(length - 1, 0, -1) if string[index] > string[index - 1]), None)
+        yield string
+
+        left = next((index - 1 for index in xrange(length - 1, 0, -1)
+                     if string[index] > string[index - 1]),
+                    None)
+
         if left is None:
-            return        
-        right = next((index for index in xrange(length - 1, left, -1) if string[left] < string[index]), left + 1)
+            return
+
+        right = next((index
+                      for index in xrange(length - 1, left, -1)
+                      if string[left] < string[index]),
+                     left + 1)
+
         # Swap left and right characters, then reverse everything after left's original location.
         string = (string[:left]
                   + string[right:right + 1]
                   + (string[left + 1:right] + string[left:left + 1] + string[right + 1:])[::-1])
 
 
-sys.stdout.writelines(','.join(permutations(word.rstrip())) + '\n' for word in open(sys.argv[1], 'r'))
+sys.stdout.writelines(','.join(permutations(word.rstrip())) + '\n'
+                      for word in open(sys.argv[1], 'r'))
