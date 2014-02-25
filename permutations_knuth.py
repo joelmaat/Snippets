5dce9fc1774f2d9846815ba0bf56d54b37dcbc69
Joel Johnson <joelmaat@gmail.com>
2014-02-24 20:56:39 -0500

    Update permutations_knuth.py

--- permutations_knuth.py
+++ permutations_knuth.py
@@ -9,16 +9,19 @@ def permutations(string):
     length = len(string)
     while True:
         yield string
-        left = next((index - 1 
-                     for index in reversed(xrange(1, length))
-                     if string[index] > string[index - 1]),
-                    None)
-        if left is None:
-            return
-        right = next((index
-                      for index in reversed(xrange(left + 2, length))
-                      if string[left] < string[index]),
-                     left + 1)
+        
+        for index in reversed(xrange(1, length)):
+            if string[index] > string[index - 1]):
+                left = index - 1
+                break
+        else: return
+    
+        for index in reversed(xrange(left + 2, length)):
+            if string[left] < string[index]:
+                right = index
+                break
+        else: right = left + 1
+            
         # Swap left and right characters, then reverse everything after left's original location.
         string = (string[:left]
                   + string[right:right + 1]
