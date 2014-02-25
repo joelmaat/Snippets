a19e70089e7ca49e6f710d90bdfa31d7159cc6ce
Joel Johnson <joelmaat@gmail.com>
2014-02-24 21:00:07 -0500

    Whitespace

--- permutations_knuth.py
+++ permutations_knuth.py
@@ -9,19 +9,18 @@ def permutations(string):
     length = len(string)
     while True:
         yield string
-        
         for index in reversed(xrange(1, length)):
             if string[index] > string[index - 1]:
                 left = index - 1
                 break
-        else: return
-    
+        else: 
+            return
         for index in reversed(xrange(left + 2, length)):
             if string[left] < string[index]:
                 right = index
                 break
-        else: right = left + 1
-            
+        else: 
+            right = left + 1
         # Swap left and right characters, then reverse everything after left's original location.
         string = (string[:left]
                   + string[right:right + 1]
